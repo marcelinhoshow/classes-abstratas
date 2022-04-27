f9d92b6d8b450021803a2f00f1ba58b118391dc5
Marcelo Jordão <44094119+marcelinhoshow@users.noreply.github.com>
2022-04-27 17:27:15 -0300

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,14 @@
+public abstract class Animal{
+
+    private String nome;
+    public abstract String emitirSom();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+ 
+}
---
+++ Papagaio.java
@@ -0,0 +1,24 @@
+public class Papagaio extends Ave {
+
+    @Override
+    public String voar(){
+        //TODO Auto-generated method stub
+        return "Voa mal";
+    }
+
+    @Override
+    public String falar() {
+        // TODO Auto-generated method stub
+        return "Fala muito";
+    }
+    
+    @Override
+    public String emitirSom() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void mensagem(){
+        System.out.println("");
+    }
+}
